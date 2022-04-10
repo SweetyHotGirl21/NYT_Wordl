17ed01e679c4f1f58b1d8a2e7d49bd05e9cd6774
SweetyHotGirl21 <54152570+SweetyHotGirl21@users.noreply.github.com>
2022-04-10 15:50:09 +0200

    new data

--- functions/functions.py
+++ functions/functions.py
@@ -4,14 +4,15 @@ import numpy as np
 
 # select words of lenght 5
 def by_size(words, size):
-    # l = filter(lambda x: len(x)==size, words)
-    return [word for word in words if len(word) == size]
+    size_list = [word for word in words if len(word) == size]
+    alpha_list = [i for i in size_list if i.isalpha()] 
+    return alpha_list
 
 
 # create dictionary with counted letters
 def count_letters(l):
     letter = dict() 
-    letters = ["first","second","third","four","five"]
+    letters = range(5)
     for x in range(5):
         letter[letters[x]] = dict()
         for word in l:
